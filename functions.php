f30faa54b584b7eacf466b484010372df0f80f0d
kelsea_b <kelseabergan@gmail.com>
2021-01-29 11:32:52 -0700

    final updates

---
+++ functions.php
@@ -0,0 +1,75 @@
+<?php
+/**
+* Accelerate Marketing Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Accelerate Marketing
+* @since Accelerate Marketing 2.0
+*/
+
+// Enqueue scripts and styles
+function accelerate_child_scripts(){
+	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+	wp_enqueue_style( 'accelerate-child-google-fonts' , '//fonts.googleapis.com/css2?family=Roboto+Mono&display');
+
+}
+add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+
+
+
+//Custom post types function
+
+function create_custom_post_types() {
+
+//create a case study post type
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ),
+        )
+    );
+//create about page template
+
+    	register_post_type( 'services',
+        array(
+            'labels' => array(
+                'name' => __( 'Services' ),
+                'singular_name' => __( 'Service' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            /*'slug' => 'about'*/
+        )
+    );
+}
+
+//Hook this custom post type function into theme
+add_action( 'init', 'create_custom_post_types' );
+
+
+//Dynamic Sidebar//
+
+function accelerate_theme_child_widget_init() {
+
+	register_sidebar( array(
+	    'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
+	    'id' => 'sidebar-2',
+	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	    'after_widget' => '</aside>',
+	    'before_title' => '<h3 class="widget-title">',
+	    'after_title' => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
